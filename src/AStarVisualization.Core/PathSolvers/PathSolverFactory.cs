b22f75ada59a32cbced2df30b3f70fcb89d9273a
Flo Rager Laptop <florianrager90@gmail.com>
2019-10-07 10:43:49 +0200

    implements PathSolverFactoryTest

---
+++ src/AStarVisualization.Core/PathSolvers/PathSolverFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace AStarVisualization.Core.PathSolvers
+{
+    public class PathSolverFactory : IPathSolverFactory
+    {
+        public IPathSolver GetPathSolver(ref Node[,] map, PathSolver pathSolverType, bool diagonalsEnabled)
+        {
+            switch (pathSolverType)
+            {
+                case PathSolver.AStar:
+                    return new AStarPathSolver(ref map, diagonalsEnabled);
+                default:
+                    return null;
+            }
+        }
+    }
+}
