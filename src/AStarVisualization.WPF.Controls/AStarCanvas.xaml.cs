d9f98d95337a9d302ab1a05857ea97874b47bc94
Rager Florian <florianrager90@gmail.com>
2019-10-07 22:09:25 +0200

    continued work on implementing the custom control

--- src/AStarVisualization.WPF.Controls/AStarCanvas.xaml.cs
+++ src/AStarVisualization.WPF.Controls/AStarCanvas.xaml.cs
@@ -10,24 +10,35 @@ namespace AStarVisualization.WPF.Controls
     /// </summary>
     public partial class AStarCanvas : UserControl
     {
+        //public static readonly DependencyProperty MapProperty =
+        //    DependencyProperty.Register(
+        //        "Map", typeof(Node[,]), typeof(AStarCanvas),
+        //    new PropertyMetadata(null));
+        //public static readonly DependencyProperty PathProperty =
+        //    DependencyProperty.Register(
+        //        "Path", typeof(List<Node>), typeof(AStarCanvas),
+        //    new PropertyMetadata(new List<Node>()));
+        //public Node[,] Map
+        //{
+        //    get => (Node[,])GetValue(MapProperty);
+        //    set => SetValue(MapProperty, value);
+        //}
+        //public List<Node> Path
+        //{
+        //    get => (List<Node>)GetValue(PathProperty);
+        //    set => SetValue(PathProperty, value);
+        //}
+
+        // snipet for bindable properties propdbd
+
+
+        public Node[,] MyProperty
+        {
+            get { return (Node[,])GetValue(MapProperty); }
+            set { SetValue(MapProperty, value); }
+        }
         public static readonly DependencyProperty MapProperty =
-            DependencyProperty.Register(
-                "Map", typeof(Node[,]), typeof(AStarCanvas),
-            new PropertyMetadata(null));
-        public static readonly DependencyProperty PathProperty =
-            DependencyProperty.Register(
-                "Path", typeof(List<Node>), typeof(AStarCanvas),
-            new PropertyMetadata(new List<Node>()));
-        public Node[,] Map
-        {
-            get => (Node[,])GetValue(MapProperty);
-            set => SetValue(MapProperty, value);
-        }
-        public List<Node> Path
-        {
-            get => (List<Node>)GetValue(PathProperty);
-            set => SetValue(PathProperty, value);
-        }
+            DependencyProperty.Register("MyProperty", typeof(Node[,]), typeof(AStarCanvas), new PropertyMetadata(0));
 
 
         public AStarCanvas()
---
+++ src/AStarVisualization.WPF.Controls/TestControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AStarVisualization.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for TestControl.xaml
+    /// </summary>
+    public partial class TestControl : UserControl
+    {
+        public static readonly DependencyProperty CustomTextProperty =
+            DependencyProperty.Register(
+                "CustomText", typeof(string), typeof(AStarCanvas),
+            new PropertyMetadata(string.Empty));
+        public string CustomText
+        {
+            get => (string)GetValue(CustomTextProperty);
+            set => SetValue(CustomTextProperty, value);
+        }
+        public TestControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
