6312d53696e9e8d9c37ff9819b13701f797fa15c
Rager Florian <florianrager90@gmail.com>
2019-10-28 22:06:01 +0100

    continues implementing placing and removal of the tiles

--- src/PathFindingVisualization.WPF/Bindings.cs
+++ src/PathFindingVisualization.WPF/Bindings.cs
@@ -8,10 +8,10 @@ namespace PathFindingVisualization.WPF
     {
         public override void Load()
         {
-            Bind<MapCanvasData>().To<MapCanvasData>().InSingletonScope();
-            Bind<MainWindow>().To<MainWindow>().InSingletonScope();
-            Bind<MainViewModel>().To<MainViewModel>().InSingletonScope();
             Bind<AlgorithmControlViewModel>().To<AlgorithmControlViewModel>().InSingletonScope();
+            Bind<MainViewModel>().To<MainViewModel>().InSingletonScope();
+            Bind<MainWindow>().To<MainWindow>().InSingletonScope();
+            Bind<MapCanvasData>().To<MapCanvasData>().InSingletonScope();
         }
     }
 }
---
+++ src/PathFindingVisualization.WPF/NodeStateChanger.cs
@@ -0,0 +1,19 @@
+﻿using PathFindingVisualization.Core.Node;
+using System.Windows.Input;
+
+namespace PathFindingVisualization.WPF
+{
+    public class NodeStateChanger
+    {
+        public ICommand HandleRightClick;
+        public ICommand HandleLeftClick;
+        public ICommand ProcessMouseMovement;
+
+        private NodeState PlacementMode = NodeState.Ground;
+
+
+        public void ChangeNodeState()
+        {
+        }
+    }
+}
