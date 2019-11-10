b4277519749b1e4155b4d7ba6c56381010f7e3e4
Rager Florian <florianrager90@gmail.com>
2019-11-10 16:04:18 +0100

    adds some of the icons (only for testing svg usage with buttons)

--- src/PathFindingVisualization.WPF/Bindings.cs
+++ src/PathFindingVisualization.WPF/Bindings.cs
@@ -13,20 +13,20 @@ namespace PathFindingVisualization.WPF
         public override void Load()
         {
             Bind<ICommand>().To<ClearMapCommand>().Named("ClearMapCommand");
+            Bind<ICommand>().To<ClearMapCommand>().Named("ClearMapCommand");
             Bind<ICommand>().To<PlaceGoalCommand>().Named("PlaceGoalCommand");
             Bind<ICommand>().To<PlaceStartCommand>().Named("PlaceStartCommand");
             Bind<ICommand>().To<PlaceTileCommand>().Named("PlaceTileCommand");
             Bind<ICommand>().To<ProcessMouseMovementCommand>().Named("ProcessMouseMovementCommand");
             Bind<ICommand>().To<RemoveTileCommand>().Named("RemoveTileCommand");
-
-            Bind<ICommand>().To<StartAlgorithmCommand>().Named("StartAlgorithmCommand"); // HACK: for testing purposes
             Bind<ICommand>().To<ResetAlgorithmCommand>().Named("ResetAlgorithmCommand");
+            Bind<ICommand>().To<StartAlgorithmCommand>().Named("StartAlgorithmCommand"); // HACK: for testing purposes
 
             Bind<AlgorithmControlViewModel>().To<AlgorithmControlViewModel>().InSingletonScope();
-            Bind<ApplicationState>().To<ApplicationState>().InSingletonScope();
-            Bind<IPathSolverFactory>().To<PathSolverFactory>().InSingletonScope();
             Bind<MainViewModel>().To<MainViewModel>().InSingletonScope();
             Bind<MainWindow>().To<MainWindow>().InSingletonScope();
+            Bind<ApplicationState>().To<ApplicationState>().InSingletonScope();
+            Bind<IPathSolverFactory>().To<PathSolverFactory>().InSingletonScope();
         }
     }
 }
