1865b06111ea1819169e495f5b8b00685de360b1
Rager Florian <florianrager90@gmail.com>
2019-12-10 07:35:32 +0100

    removed old pathsolver types

--- src/PathFindingVisualization.Core/PathSolvers/PathSolverFactory.cs
+++ src/PathFindingVisualization.Core/PathSolvers/PathSolverFactory.cs
@@ -14,8 +14,6 @@ namespace PathFindingVisualization.Core.PathSolvers
                 PathSolver.Dijkstra => new DijkstraPathSolver(map, diagonalsEnabled),
                 PathSolver.BestFirstSearch => throw new NotImplementedException(),
                 PathSolver.BreadthFirstSearch => throw new NotImplementedException(),
-                PathSolver.JumpPointSearch => throw new NotImplementedException(),
-                PathSolver.OrthogonalJumpPointSearch => throw new NotImplementedException(),
                 _ => throw new Exception($"The requested Pathsolving algorithm ({pathSolverType}) is not implemented")
             };
         }
